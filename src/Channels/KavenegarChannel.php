2d3f49aeeeb7208b533c21c74e03ad1090c3ab28
Reza Askari <askari.reza@outlook.com>
2019-04-13 16:23:38 +0430

    Kavenegar notification channel created.

---
+++ src/Channels/KavenegarChannel.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Kavenegar\Channels;
+
+
+use Illuminate\Notifications\Notification;
+use Kavenegar\Facades\Kavenegar;
+
+class KavenegarChannel
+{
+    /**
+     * Send the given notification.
+     *
+     * @param mixed $notifiable
+     * @param \Illuminate\Notifications\Notification $notification
+     *
+     * @return void
+     */
+    public function send($notifiable, Notification $notification)
+    {
+        $receptor = $notifiable->routeNotificationFor('sms');
+
+        switch ($this->detectType($notification)) {
+            case 'lookup':
+                Kavenegar::lookup($notification->getLookupTemplate(), $receptor, $notifiable->getLookupToken());
+                break;
+
+            case 'message':
+                Kavenegar::send($receptor, $notification->toSMS());
+                break;
+
+        }
+    }
+
+    /**
+     * Detect type of notification.
+     *
+     * @param Notification $notification
+     *
+     * @return string
+     */
+    public function detectType(Notification $notification)
+    {
+        if (method_exists($notification, 'getLookupTemplate'))
+            return 'lookup';
+        else
+            return 'message';
+    }
+}
