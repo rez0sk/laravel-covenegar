e26df01726b26f3f7725e85afdb8805bd95e821d
Reza Askari <askari.reza@outlook.com>
2019-04-13 16:26:22 +0430

    Method description added to kavenegar facade.

--- src/Facades/Kavenegar.php
+++ src/Facades/Kavenegar.php
@@ -3,8 +3,15 @@
 
 namespace Kavenegar\Facades;
 
+/**
+ * @method static void send ($receptor, string $message, array $options)
+ *
+ * @see \Kavenegar\Kavenegar
+ */
 
-class Kavenegar
+use Illuminate\Support\Facades\Facade;
+
+class Kavenegar extends Facade
 {
     protected static function getFacadeAccessor() { return 'kavenegar'; }
 }
